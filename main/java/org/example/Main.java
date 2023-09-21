8055f7593b232ce09f654840ac9dc6475e228852
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2023-09-21 20:27:04 -0300

    Crinado classe smartphone

---
+++ main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Smartphone iPhone = new Smartphone(
+                "Apple", "iPhone 1", (short) 2007, "128MB", "4GB"
+        );
+
+        System.out.println(iPhone);
+
+        iPhone.exibirPagina("www.google.com");
+        iPhone.atualizarPagina();
+        iPhone.adicionarNovaAba();
+        iPhone.selecionarMusica("DVRST - Dream Space");
+        iPhone.tocar();
+        iPhone.pausar();
+        iPhone.ligar("83 994444645");
+        iPhone.iniciarCorreioVoz();
+        iPhone.atender();
+    }
+}
